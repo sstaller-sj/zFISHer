7b59179420f85c32c4aff2cc8c79384b8722c15c
sstaller-sj <Seth.Staller@STJUDE.ORG>
2025-04-17 11:49:33 -0500

    Updated toml with Walid

--- zFISHer/config/config_manager.py
+++ zFISHer/config/config_manager.py
@@ -1,7 +1,24 @@
 import os
 import json
+import shutil
+
+
 
 CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), "config.json")
+
+##########
+CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".zFISHer")
+CONFIG_FILE_PATH = os.path.join(CONFIG_DIR, "config.json")
+DEFAULT_CONFIG_SOURCE = "/hpcf/authorized_apps/rhel8_apps/zfisher/vendor/zFISHer/zFISHer/config/config.json"
+
+# Ensure the config directory exists
+os.makedirs(CONFIG_DIR, exist_ok=True)
+
+# Copy the default config if it doesn't exist
+if not os.path.exists(CONFIG_FILE_PATH):
+    shutil.copy(DEFAULT_CONFIG_SOURCE, CONFIG_FILE_PATH)
+###########
+
 
 def load_config():
     """Load the configuration from the JSON file."""
